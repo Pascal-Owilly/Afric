404473279c44ecb507f564ca8231e5d3b04ba735
Pascal-Owilly <pascalouma54@gmail.com>
2023-03-24 23:46:04 +0300

    changes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,51 +1,133 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../img/logo.jpg';
-import { Link } from 'react-router-dom';
 import './NavigationBar.css';
+import logo from '../img/logo.jpg'
 
 function NavigationBar() {
-  const [expanded, setExpanded] = useState(false);
 
-  const toggleExpanded = () => {
-    setExpanded(!expanded);
-  };
 
+  // logo
+  const [logoImg, setLogoImg] = useState([logo]);
+  
+  // end logo 
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.pageYOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <>
-      <style>
-        {`.navbar-toggler-icon {
-          background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke='rgb(255, 255, 255)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
-        }`}
-      </style>
-      <Navbar className='navbar' fixed='top' expand="lg" expanded={expanded}>
-        <Container fluid>
-          <Navbar.Brand>  
-            <Link to="/">
-              <img
-                src={logo}
-                width="75"
-                height="70"
-                className="d-inline-block align-top rounded"
-                alt="Mwani Africa Logo"
-              />
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleExpanded}   />
-          <Navbar.Collapse id="basic-navbar-nav" className='text-center' style={{ border:'none', height:'8vh', width:'90%', color:'', flexDirection:'row !important' }}>
-            <Nav className="justify-content-end" style={{ width: "100%" }}>
-              <Nav.Link as={Link} onClick={() => { setExpanded(false); window.scrollTo(0, 0); }}  to="/" className="nav-links" style={{fontWeight:'bold'}}>Home</Nav.Link>
-              <Nav.Link as={Link} onClick={() => { setExpanded(false); window.scrollTo(0, 0); }}  to="/about" className="nav-links" style={{fontWeight:'bold'}}>About</Nav.Link>
-              <Nav.Link as={Link} onClick={() => { setExpanded(false); window.scrollTo(0, 0); }}  to="/team" className="nav-links" style={{fontWeight:'bold'}}>The Team</Nav.Link>
-              <Nav.Link as={Link} onClick={() => { setExpanded(false); window.scrollTo(0, 0); }}  to="/socialent" className="nav-links" style={{fontWeight:"bold"}}>Social Enterprise</Nav.Link>
-              <Nav.Link as={Link} onClick={() => { setExpanded(false); window.scrollTo(0, 0); }}  to="/contact" className="nav-links" style={{fontWeight:'bold'}}>Contact</Nav.Link>            
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
-}
-
-export default NavigationBar
+    <nav
+      style={{
+        backgroundColor: scrollPosition > 100 ? 'green' : 'transparent',
+        
+        color: scrollPosition > 100 ? '#fff' : '#efefef ',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100%',
+        height: scrollPosition > 100 ? '50px' : '100px',
+        transition: 'all 0.3s ease-in-out',
+        boxShadow: scrollPosition > 100 ? '0 2px 10px rgba(0, 0, 0, 0.1)' : 'none',
+        zIndex: '9999',
+        transitionDuration: '0.3s'
+        }}
+        >
+        <div
+        style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        maxWidth: '1200px',
+        margin: '0 auto',
+        padding: '0 20px',
+        height: '100%',
+        }}
+        >
+        <a href="/" style={{ display: 'flex', alignItems: 'center', textDecoration:'none' }}>
+        <img
+        src={logo}
+        alt="Logo"
+        style={{ height: '45px', marginLeft: '-4.8rem' }}
+        />
+              
+        </a>
+        <ul
+        style={{
+        display: 'flex',
+        alignItems: 'center',
+        listStyle: 'none',
+        margin: '0',
+        padding: '0',
+        }}
+        >
+        <li>
+        <a
+        href="/"
+        style={{
+        fontWeight: 'bold',
+        color: 'inherit',
+        textDecoration: 'none',
+        padding: '20px',
+        
+        }}
+        >
+        Home
+        </a>
+        </li>
+        <li>
+        <a
+        href="/"
+        style={{
+        fontWeight: 'bold',
+        color: 'inherit',
+        textDecoration: 'none',
+        padding: '20px',
+        }}
+        >
+        About
+        </a>
+        </li>
+        <li>
+        <a
+        href="/about"
+        style={{
+        fontWeight: 'bold',
+        color: 'inherit',
+        textDecoration: 'none',
+        padding: '20px',
+        }}
+        >
+        Contact
+        </a>
+        </li>
+        <li>
+        <a
+        href="/"
+        style={{
+        fontWeight: 'bold',
+        color: 'inherit',
+        textDecoration: 'none',
+        padding: '20px',
+        }}
+        >
+        Explore
+        </a>
+        </li>
+        </ul>
+        </div>
+        {/* <hr className='hr-nav' /> */}
+        </nav>
+       
+        );
+        }
+        
+        export default NavigationBar;
